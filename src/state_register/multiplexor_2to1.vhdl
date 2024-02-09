b8d18400d66605d937ba6881bfa7a7d2e7e454d1
CLopMan <cesarlopman@gmail.com>
2024-02-09 21:03:05 +0100

    style guidelines

--- src/state_register/multiplexor_2to1.vhdl
+++ src/state_register/multiplexor_2to1.vhdl
@@ -1,29 +1,25 @@
-LIBRARY ieee; -- biblioteca
-USE ieee.std_logic_1164.all;
+library ieee; -- biblioteca
+use ieee.std_logic_1164.all;
 
---
---
---
 --                    ___
 --     in_data1 -32- |   |
 --     in_data2 -32- |   | -32- oud_data
 --         selec -1- |___|
---
---
---
 
-entity multiplexor2 is 
-    generic( constant size: integer := 32);
+entity Multiplexor2To1 is 
+    generic(constant size: integer := 32);
+    
     port(
         in_data1: in std_logic_vector (size - 1 downto 0); 
         in_data2: in std_logic_vector (size - 1 downto 0); 
         selec: in std_logic;
         out_data: out std_logic_vector(size - 1 downto 0)
-
     );
-end multiplexor2;
 
-architecture behaviour of multiplexor2 is
+end Multiplexor2To1;
+
+
+architecture behaviour of Multiplexor2To1 is
     begin process(selec)
         begin case selec is 
             when '1' => out_data <= in_data1;
