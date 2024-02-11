fffd92c392cdc8921a93c28c0cd802bbeba59625
CLopMan <cesarlopman@gmail.com>
2024-02-11 11:13:22 +0100

    encoding guidelines

---
+++ src/state_register/state_register.vhdl
@@ -0,0 +1,12 @@
+use library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity StateRegister is 
+    generic(constant SIZE: integer := 32);
+
+    port(
+        
+    );
+
+end StateRegister;
