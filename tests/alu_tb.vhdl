1423fd3c7cc134aff21d3cf3623db0f5fc73f71b
ALVAROPING1 <alvaroping1@gmail.com>
2024-02-12 12:35:02 +0100

    refactor(ALU): replace `op_code`'s type with an enum

--- tests/alu_tb.vhdl
+++ tests/alu_tb.vhdl
@@ -12,7 +12,7 @@ end ALUTB;
 
 architecture Rtl of ALUTB is
     signal A, B: signed(31 downto 0) := (others => '0');
-    signal op_code: std_logic_vector(4 downto 0) := (others => '0');
+    signal op_code: op_code_name := noop;
     signal C: signed(31 downto 0) := (others => '0');
     signal state: state_type := (others => '0');
 begin
@@ -23,7 +23,7 @@ begin
         type test_case is record
             -- Inputs
             A, B: signed(31 downto 0);
-            op_code: std_logic_vector(4 downto 0);
+            op_code: op_code_name;
             -- Expected output
             C: signed(31 downto 0);
             state: state_type;
@@ -32,77 +32,77 @@ begin
         type tests_array is array (natural range <>) of test_case;
         constant TESTS : tests_array := (
             ( -- 1: No-Op test. Always results in 0
-                x"00000001", x"00000003", "00000",
+                x"00000001", x"00000003", noop,
                 x"00000000", (Zero => '1', others => '0')
             ),
             ( -- 2: Bitwise AND test
                 "00000000000000000000000000000001",
-                "00000000000000000000000000000011", "00001",
+                "00000000000000000000000000000011", land,
                 "00000000000000000000000000000001", (others => '0')
             ),
             ( -- 3: Bitwise OR test
                 "00000000000000000000000000000011",
-                "00000000000000000000000000000101", "00010",
+                "00000000000000000000000000000101", lor,
                 "00000000000000000000000000000111", (others => '0')
             ),
             ( -- 4: Bitwise XOR test
                 "00000000000000000000000000000011",
-                "00000000000000000000000000000101", "00011",
+                "00000000000000000000000000000101", lxor,
                 "00000000000000000000000000000110", (others => '0')
             ),
             ( -- 5: Bitwise NOT A test
                 "00000000000000000000000000010101",
-                "00000000000000000000000000000010", "00100",
+                "00000000000000000000000000000010", lnot,
                 "11111111111111111111111111101010",
                 (Zero => '0', others => '1')
             ),
             ( -- 6: Addition test
-                to_signed(3, 32), to_signed(5, 32), "00101",
+                to_signed(3, 32), to_signed(5, 32), add,
                 to_signed(8, 32), (others => '0')
             ),
             ( -- 7: Subtraction test
-                to_signed(5, 32), to_signed(3, 32), "00110",
+                to_signed(5, 32), to_signed(3, 32), sub,
                 to_signed(2, 32), (others => '0')
             ),
             ( -- 8: Logical shift left test
                 "01000000000000000000000000000010",
-                "00000000000000000000000000000001", "00111",
+                "00000000000000000000000000000001", shift_ll,
                 "10000000000000000000000000000100",
                 (Negative => '1', Overflow => '1', others => '0')
             ),
             ( -- 9: Logical shift left test by more than the word size
                 "11111111111111111111111111111111",
-                "00000000000000000000000000100000", "00111",
+                "00000000000000000000000000100000", shift_ll,
                 "00000000000000000000000000000000",
                 (Zero => '1', Carry => '1', others => '0')
             ),
             ( -- 10: Logical shift right test with MSB unset
                 "01000000000000000000000000000010",
-                "00000000000000000000000000000001", "01000",
+                "00000000000000000000000000000001", shift_lr,
                 "00100000000000000000000000000001", (others => '0')
             ),
             ( -- 11: Logical shift right test with MSB set
                 "11000000000000000000000000000010",
-                "00000000000000000000000000000001", "01000",
+                "00000000000000000000000000000001", shift_lr,
                 "01100000000000000000000000000001", (others => '0')
             ),
             ( -- 12: Logical shift right test by more than the word size
                 "11111111111111111111111111111111",
-                "00000000000000000000000000100000", "01000",
+                "00000000000000000000000000100000", shift_lr,
                 "00000000000000000000000000000000",
                 (Zero => '1', others => '0')
             ),
-            ( -- 13: Negative result test (with subtraction)
-                to_signed(5, 32), to_signed(6, 32), "00110",
+            ( -- 13: Negative result test
+                to_signed(5, 32), to_signed(6, 32), sub,
                 to_signed(-1, 32),
                 (Negative => '1', Carry => '1', others => '0')
             ),
-            ( -- 14: Carry result test (with addition)
-                "11111111111111111111111111111111", to_signed(1, 32), "00101",
+            ( -- 14: Carry result test
+                "11111111111111111111111111111111", to_signed(1, 32), add,
                 to_signed(0, 32), (Zero => '1', Carry => '1', others => '0')
             ),
-            ( -- 15: Overflow result test (with addition)
-                "01111111111111111111111111111111", to_signed(1, 32), "00101",
+            ( -- 15: Overflow result test
+                "01111111111111111111111111111111", to_signed(1, 32), add,
                 "10000000000000000000000000000000",
                 (Negative => '1', Overflow => '1', others => '0')
             )
