4bb39bfcc5558f1cc666bdc79b79e1cca3660bd3
ALVAROPING1 <alvaroping1@gmail.com>
2024-02-14 12:57:41 +0100

    refactor(run): move all building logic to makefile

--- run.sh
+++ run.sh
@@ -6,34 +6,23 @@ UPDATE_PERIOD="1s"
 MAKE_ARGS='--silent'
 
 # Check parameters
-if [[ "$0" == "" ]]; then
-    echo -e "\033[31;1mError:\033[0m Missing script file path"
-    exit 1
-fi
 if [[ "$1" == "" ]]; then
     echo -e "\033[31;1mError:\033[0m Missing entity to simulate"
-    exit 2
+    exit 1
 fi
 
-CWD=$(dirname "$0")
-export WORK="$CWD/work"
-# Create output directory if it doesn't exist
-if [ ! -d "$WORK" ]; then
-    mkdir "$WORK"
-fi
-
-export TARGET="$1"
-make --always-make $MAKE_ARGS run
+TARGET="$1"
+make --always-make $MAKE_ARGS run $TARGET
 if [[ "$2" == "" ]]; then
     exit 0;
 fi
 
 # Periodically re-execute the simulation as needed
 while [[ true ]]; do
-    make -q $MAKE_ARGS
+    make -q $MAKE_ARGS $TARGET
     if [[ "$?" == "1" ]]; then # Output is outdated, regenerate it
         echo "" # Add a blank line as separator
-        make $MAKE_ARGS run
+        make $MAKE_ARGS run $TARGET
     fi
     sleep $UPDATE_PERIOD
 done
