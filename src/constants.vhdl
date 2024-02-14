fe20f033ac20f9586903b2dd49761ee9f90e57d3
ALVAROPING1 <alvaroping1@gmail.com>
2024-02-14 11:44:29 +0100

    refactor(constants): moved constants to dedicated package

---
+++ src/constants.vhdl
@@ -0,0 +1,4 @@
+package Constants is
+    constant WORD_SIZE: positive := 32;
+    constant REG_ADDR_SIZE: positive := 5;
+end package Constants;
--- src/register_bank.vhdl
+++ src/register_bank.vhdl
@@ -2,23 +2,21 @@ library IEEE;
 use IEEE.Std_Logic_1164.all;
 use IEEE.Numeric_Std.all;
 
+use work.Constants;
+
 entity RegisterBank is
-    generic (
-        word_size: positive := 32;
-        addr_size: positive := 5
-    );
     port (
-        signal RA, RB, RC: in unsigned(addr_size - 1 downto 0);
-        signal C: in std_logic_vector(word_size - 1 downto 0);
+        signal RA, RB, RC: in unsigned(Constants.REG_ADDR_SIZE - 1 downto 0);
+        signal C: in std_logic_vector(Constants.WORD_SIZE - 1 downto 0);
         signal clk, rst, load: in std_logic;
-        signal A, B: out std_logic_vector(word_size - 1 downto 0)
+        signal A, B: out std_logic_vector(Constants.WORD_SIZE - 1 downto 0)
     );
 end entity RegisterBank;
 
 
 architecture Rtl of RegisterBank is
-    type reg_state is array(natural range 0 to 2**addr_size - 1)
-                   of std_logic_vector(word_size - 1 downto 0);
+    type reg_state is array(natural range 0 to 2**Constants.REG_ADDR_SIZE - 1)
+                   of std_logic_vector(Constants.WORD_SIZE - 1 downto 0);
     signal state: reg_state := (others => (others => '0'));
 begin
     update_state: process(clk, rst)
