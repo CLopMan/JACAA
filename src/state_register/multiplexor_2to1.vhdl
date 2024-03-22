5545cf06d9dcb1db4488a9dad3d3dd06a5bd1c3f
ALVAROPING1 <alvaroping1@gmail.com>
2024-03-22 16:49:01 +0100

    chore(format): formatted code

--- src/state_register/multiplexor_2to1.vhdl
+++ src/state_register/multiplexor_2to1.vhdl
@@ -1,30 +1,29 @@
 library ieee;
-use work.Constants;
 use ieee.std_logic_1164.all;
+
+use work.Constants;
+
 --                    ___
 --     in_data1 -32- |   |
 --     in_data2 -32- |   | -32- oud_data
 --         selec -1- |___|
-entity Multiplexor2To1 is 
-    
+entity Multiplexor2To1 is
     port(
-        in_data1: in std_logic_vector (Constants.WORD_SIZE - 1 downto 0); 
-        in_data0: in std_logic_vector (Constants.WORD_SIZE - 1 downto 0); 
+        in_data1: in std_logic_vector (Constants.WORD_SIZE - 1 downto 0);
+        in_data0: in std_logic_vector (Constants.WORD_SIZE - 1 downto 0);
         selec: in std_logic;
         out_data: out std_logic_vector(Constants.WORD_SIZE - 1 downto 0)
     );
-
 end Multiplexor2To1;
 
 
 architecture behaviour of Multiplexor2To1 is
-begin 
+begin
     process(selec, in_data0, in_data1)
-        begin case selec is 
+        begin case selec is
             when '1' => out_data <= in_data1;
             when '0' => out_data <= in_data0;
             when others => out_data <= (others => 'X');
         end case;
     end process;
-
 end architecture behaviour;
--- tests/register_tb.vhdl
+++ tests/register_tb.vhdl
@@ -3,66 +3,61 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 library Src;
+use Src.Constants;
 
 entity RegisterTB is
-    generic(constant size: integer := 32);
 end RegisterTB;
 
 
-architecture behavior of RegisterTB is 
-	signal s_clk : std_logic := '0';
+architecture behavior of RegisterTB is
+    constant SIZE: positive := Constants.WORD_SIZE;
+
+	signal clk : std_logic := '0';
 	signal s_rst : std_logic;
 	signal s_update : std_logic;
-    signal s_in_data: std_logic_vector(size - 1 downto 0);
-    signal s_out_data: std_logic_vector(size - 1 downto 0);
-    signal kill_clock: std_logic := '0';
-begin 
+    signal s_in_data: std_logic_vector(SIZE - 1 downto 0);
+    signal s_out_data: std_logic_vector(SIZE - 1 downto 0);
+    signal clk_kill: std_logic := '0';
+begin
     uut: entity Src.Reg port map ( -- unit under test
-        clk => s_clk,
+        clk => clk,
         rst => s_rst,
         update => s_update,
         out_data => s_out_data,
         in_data => s_in_data
     );
 
-    clock: process
-        begin 
-            wait for 5 ns;
-            s_clk <= not s_clk; 
-            if (kill_clock = '1') then wait; 
-            end if;
-        end process;
+    clock: entity work.Clock port map (clk_kill, clk);
 
-    stim_proc: process --stimulation process 
-    begin 
-        
+    stim_proc: process -- stimulation process
+    begin
         -- test1: store value
-        s_in_data <= std_logic_vector(to_unsigned(92, size));
-        s_update <= '1'; 
-        wait for 10 ns; assert s_out_data = std_logic_vector(to_unsigned(92, size)) report "failed store 92: test 1";
-        report "value: " & integer'image(to_integer(signed(s_out_data)));
-
-        -- test2: reset value 
-        s_rst <= '1'; 
-        wait for 10 ns; assert s_out_data = std_logic_vector(to_unsigned(0, size)) report "failed rested: test 2";
-        report "value: " & integer'image(to_integer(signed(s_out_data)));
-
-        -- test3: read value while writing 
+        s_in_data <= std_logic_vector(to_unsigned(92, SIZE));
         s_update <= '1';
-        s_rst <= '0'; 
-        s_in_data <= std_logic_vector(to_unsigned(33, size));
-        assert s_out_data = std_logic_vector(to_unsigned(0, size)) report "failed read-while-writing: test 3";
+        wait for 10 ns; assert s_out_data = std_logic_vector(to_unsigned(92, SIZE)) report "failed store 92: test 1";
         report "value: " & integer'image(to_integer(signed(s_out_data)));
-        wait for 10 ns; assert s_out_data = std_logic_vector(to_unsigned(33, size)) report "failed read-while-writing fase 2: test 3";
+
+        -- test2: reset value
+        s_rst <= '1';
+        wait for 10 ns; assert s_out_data = std_logic_vector(to_unsigned(0, SIZE)) report "failed rested: test 2";
         report "value: " & integer'image(to_integer(signed(s_out_data)));
-        -- test4: read twice 
-        wait for 10 ns; assert s_out_data = std_logic_vector(to_unsigned(33, size)) report "failed store 33: test 4";
+
+        -- test3: read value while writing
+        s_update <= '1';
+        s_rst <= '0';
+        s_in_data <= std_logic_vector(to_unsigned(33, SIZE));
+        assert s_out_data = std_logic_vector(to_unsigned(0, SIZE)) report "failed read-while-writing: test 3";
         report "value: " & integer'image(to_integer(signed(s_out_data)));
-        wait for 10 ns; assert s_out_data = std_logic_vector(to_unsigned(33, size)) report "failed store 33: test 4";
+        wait for 10 ns; assert s_out_data = std_logic_vector(to_unsigned(33, SIZE)) report "failed read-while-writing fase 2: test 3";
         report "value: " & integer'image(to_integer(signed(s_out_data)));
-    
+        -- test4: read twice
+        wait for 10 ns; assert s_out_data = std_logic_vector(to_unsigned(33, SIZE)) report "failed store 33: test 4";
+        report "value: " & integer'image(to_integer(signed(s_out_data)));
+        wait for 10 ns; assert s_out_data = std_logic_vector(to_unsigned(33, SIZE)) report "failed store 33: test 4";
+        report "value: " & integer'image(to_integer(signed(s_out_data)));
+
         report "finish";
-        kill_clock <= '1';
+        clk_kill <= '1';
         wait;
     end process;
 end behavior;
