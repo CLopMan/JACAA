5a435be0f8caecf2b3be2ce4e450f6ce1bfed07b
ALVAROPING1 <alvaroping1@gmail.com>
2024-03-24 23:32:38 +0100

    fix(ALU): use modulo for the second argument of logical shifts
    This is the specified behavior in the RISC-V standard

--- tests/alu_tb.vhdl
+++ tests/alu_tb.vhdl
@@ -66,42 +66,60 @@ begin
             ),
             ( -- 8: Logical shift left test
                 "01000000000000000000000000000010",
-                "00000000000000000000000000000001", shift_ll,
+                to_signed(1, 32), shift_ll,
                 "10000000000000000000000000000100",
                 (Negative => '1', Overflow => '1', others => '0')
             ),
-            ( -- 9: Logical shift left test by more than the word size
+            ( -- 9: Logical shift left test by the word size
                 "11111111111111111111111111111111",
-                "00000000000000000000000000100000", shift_ll,
-                "00000000000000000000000000000000",
-                (Zero => '1', Carry => '1', others => '0')
+                to_signed(32, 32), shift_ll,
+                "11111111111111111111111111111111",
+                (Negative => '1', others => '0')
             ),
-            ( -- 10: Logical shift right test with MSB unset
+            ( -- 10: Logical shift left test by more than the word size with MSB unset
+                "01111111111111111111111111111111",
+                to_signed(33, 32), shift_ll,
+                "11111111111111111111111111111110",
+                (Negative => '1', Overflow => '1', others => '0')
+            ),
+            ( -- 11: Logical shift left test by more than the word size with MSB set
+                "11111111111111111111111111111111",
+                to_signed(33, 32), shift_ll,
+                "11111111111111111111111111111110",
+                (Negative => '1', Carry => '1', others => '0')
+            ),
+            ( -- 12: Logical shift right test with MSB unset
                 "01000000000000000000000000000010",
-                "00000000000000000000000000000001", shift_lr,
+                to_signed(1, 32), shift_lr,
                 "00100000000000000000000000000001", (others => '0')
             ),
-            ( -- 11: Logical shift right test with MSB set
+            ( -- 13: Logical shift right test with MSB set
                 "11000000000000000000000000000010",
-                "00000000000000000000000000000001", shift_lr,
+                to_signed(1, 32), shift_lr,
                 "01100000000000000000000000000001", (others => '0')
             ),
-            ( -- 12: Logical shift right test by more than the word size
+            ( -- 14: Logical shift right test by the word size
                 "11111111111111111111111111111111",
-                "00000000000000000000000000100000", shift_lr,
-                "00000000000000000000000000000000",
-                (Zero => '1', others => '0')
+                to_signed(32, 32), shift_lr,
+                "11111111111111111111111111111111",
+                (Negative => '1', others => '0')
             ),
-            ( -- 13: Negative result test
+            ( -- 15: Logical shift right test by more than the word size
+                "11111111111111111111111111111111",
+                to_signed(33, 32), shift_lr,
+                "01111111111111111111111111111111",
+                (others => '0')
+            ),
+            ( -- 16: Negative result test
                 to_signed(5, 32), to_signed(6, 32), sub,
                 to_signed(-1, 32),
                 (Negative => '1', Carry => '1', others => '0')
             ),
-            ( -- 14: Carry result test
+            ( -- 17: Carry result test
                 "11111111111111111111111111111111", to_signed(1, 32), add,
                 to_signed(0, 32), (Zero => '1', Carry => '1', others => '0')
             ),
-            ( -- 15: Overflow result test
+            ( -- 18: Overflow result test
                 "01111111111111111111111111111111", to_signed(1, 32), add,
                 "10000000000000000000000000000000",
                 (Negative => '1', Overflow => '1', others => '0')
@@ -120,6 +138,10 @@ begin
             -- Check the outputs
             assert C = TESTS(i).C
                 report "bad ALU result on test: " & integer'image(i + 1)
+                    & ", result: "
+                    & integer'image(to_integer(signed(C)))
+                    & ", expected: "
+                    & integer'image(to_integer(signed(TESTS(i).C)))
                 severity error;
             assert state = TESTS(i).state
                 report "bad state result on test: " & integer'image(i + 1)
