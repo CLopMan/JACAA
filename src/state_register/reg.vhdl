c90405b00d2e7607705d122716ea9175f4a0f5a8
CLopMan <cesarlopman@gmail.com>
2024-07-01 16:59:48 +0200

    Generic register

--- src/state_register/reg.vhdl
+++ src/state_register/reg.vhdl
@@ -1,26 +1,30 @@
-library ieee; -- biblioteca
-use ieee.std_logic_1164.all;
+library IEEE;
+use IEEE.Std_Logic_1164.all;
 
 use work.Constants;
 
 entity Reg is
+    generic(
+        reg_size: positive := Constants.WORD_SIZE;
+        updt_rising: std_logic := '1'
+    );
     port(
         clk: in std_logic;
         rst: in std_logic;
         update: in std_logic;
-        in_data: in std_logic_vector(Constants.WORD_SIZE - 1 downto 0);
-        out_data: out std_logic_vector(Constants.WORD_SIZE - 1 downto 0)
+        in_data: in std_logic_vector(reg_size - 1 downto 0);
+        out_data: out std_logic_vector(reg_size - 1 downto 0)
     );
 end Reg;
 
 
 architecture Behaviour of Reg is
-    signal keeped_data: std_logic_vector (Constants.WORD_SIZE - 1 downto 0);
+    signal keeped_data: std_logic_vector (reg_size - 1 downto 0);
 begin
     process (clk, rst)
     begin
         if rst = '1' then keeped_data <= (others => '0');
-        elsif rising_edge(clk) then
+        elsif clk'event and clk = updt_rising then
             if update = '1' then keeped_data <= in_data;
             end if;
         end if;
--- tests/register_tb.vhdl
+++ tests/register_tb.vhdl
@@ -3,39 +3,61 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 library Src;
-use Src.Constants;
 
-entity RegisterTB is
-end RegisterTB;
+entity RegTB is
+end RegTB;
 
 
-architecture behavior of RegisterTB is
-    constant SIZE: positive := Constants.WORD_SIZE;
+architecture behavior of RegTB is
+    constant SIZE32: positive := 32;
+    constant SIZE5: positive := 5;
 
 	signal clk : std_logic := '0';
 	signal s_rst : std_logic;
 	signal s_update : std_logic;
-    signal s_in_data: std_logic_vector(SIZE - 1 downto 0);
-    signal s_out_data: std_logic_vector(SIZE - 1 downto 0);
+    signal s_in_data_32: std_logic_vector(SIZE32 - 1 downto 0);
+    signal s_out_data_32: std_logic_vector(SIZE32 - 1 downto 0);
+    signal s_in_data_5: std_logic_vector(SIZE5 - 1 downto 0);
+    signal s_out_data_5: std_logic_vector(SIZE5 - 1 downto 0);
     signal clk_kill: std_logic := '0';
 begin
-    uut: entity Src.Reg port map ( -- unit under test
-        clk => clk,
-        rst => s_rst,
-        update => s_update,
-        out_data => s_out_data,
-        in_data => s_in_data
-    );
+    uut1: entity Src.Reg -- reg 32 updated rising edge
+        generic map(
+            reg_size => SIZE32,
+            updt_rising => '1'
+        )
+        port map ( -- unit under test
+            clk => clk,
+            rst => s_rst,
+            update => s_update,
+            out_data => s_out_data_32,
+            in_data => s_in_data_32
+        );
+
+    uut2: entity Src.Reg -- reg 5 updated falling edge
+        generic map(
+            reg_size => SIZE5,
+            updt_rising => '0'
+        )
+        port map ( -- unit under test
+            clk => clk,
+            rst => s_rst,
+            update => s_update,
+            out_data => s_out_data_5,
+            in_data => s_in_data_5
+        );
 
     clock: entity work.Clock port map (clk_kill, clk);
 
     stim_proc: process -- stimulation process
         type test_case is record
             -- inputs
-            s_in_data: std_logic_vector(SIZE - 1 downto 0);
+            s_in_data_32: std_logic_vector(SIZE32 - 1 downto 0);
+            s_in_data_5: std_logic_vector(SIZE5 - 1 downto 0);
             s_update, s_rst: std_logic;
             -- output
-            s_out_data: std_logic_vector(SIZE - 1 downto 0);
+            s_out_data_32: std_logic_vector(SIZE32 - 1 downto 0);
+            s_out_data_5: std_logic_vector(SIZE5 - 1 downto 0);
         end record;
 
         type tests_array is array (natural range <>) of test_case;
@@ -43,44 +65,78 @@ begin
         constant TESTS: tests_array := (
             -- test1: store value
             (
-                std_logic_vector(to_unsigned(92, SIZE)), '1', '0',
-                std_logic_vector(to_unsigned(92, SIZE))
+                std_logic_vector(to_unsigned(92, SIZE32)),
+                std_logic_vector(to_unsigned(2, SIZE5)),
+                '1', '0',
+                std_logic_vector(to_unsigned(92, SIZE32)),
+                std_logic_vector(to_unsigned(2, SIZE5))
             ),
             -- test2: reset value
             (
-                std_logic_vector(to_unsigned(92, SIZE)), '0', '1',
-                std_logic_vector(to_unsigned(0, SIZE))
+                std_logic_vector(to_unsigned(92, SIZE32)),
+                std_logic_vector(to_unsigned(31, SIZE5)),
+                '0', '1',
+                std_logic_vector(to_unsigned(0, SIZE32)),
+                std_logic_vector(to_unsigned(0, SIZE5))
             ),
-            -- test3: read value while writing
+            -- test3: update value
             (
-                std_logic_vector(to_unsigned(33, SIZE)), '1', '0',
-                std_logic_vector(to_unsigned(33, SIZE))
+                std_logic_vector(to_unsigned(33, SIZE32)),
+                std_logic_vector(to_unsigned(1, SIZE5)),
+                '1', '0',
+                std_logic_vector(to_unsigned(33, SIZE32)),
+                std_logic_vector(to_unsigned(1, SIZE5))
             ),
             -- test4: read twice
             (
-                std_logic_vector(to_unsigned(0, SIZE)), '0', '0',
-                std_logic_vector(to_unsigned(33, SIZE))
+                std_logic_vector(to_unsigned(0, SIZE32)),
+                std_logic_vector(to_unsigned(0, SIZE5)),
+                '0', '0',
+                std_logic_vector(to_unsigned(33, SIZE32)),
+                std_logic_vector(to_unsigned(1, SIZE5))
             )
         );
     begin
-        assert false report "start of test" severity note;
+        report "start of test" severity note;
+        wait for 5 ns; -- half of a cicle to ensure falling reg updates
         for i in TESTS'range loop
             -- Set the inputs
-            s_in_data <= TESTS(i).s_in_data;
+            s_in_data_32 <= TESTS(i).s_in_data_32;
+            s_in_data_5 <= TESTS(i).s_in_data_5;
             s_update <= TESTS(i).s_update;
             s_rst <= TESTS(i).s_rst;
+            -- before wait prev value must be readed
+            if i > 0 then
+                assert s_out_data_32 = TESTS(i - 1).s_out_data_32
+                    and s_out_data_5 = TESTS(i - 1).s_out_data_5
+                report "bad result on test: " & integer'image(i + 1)
+                    & " before waiting, result32: "
+                    & integer'image(to_integer(signed(s_out_data_32)))
+                    & ", expected32: "
+                    & integer'image(to_integer(signed(TESTS(i).s_out_data_32)))
+                    & ", result5: "
+                    & integer'image(to_integer(signed(s_out_data_5)))
+                    & ", expected5: "
+                    & integer'image(to_integer(signed(TESTS(i).s_out_data_5)))
+                severity error;
+            end if;
             -- Wait for the results
             wait for 10 ns;
             -- Check the outputs
-            assert s_out_data = TESTS(i).s_out_data
+            assert s_out_data_32 = TESTS(i).s_out_data_32
+            and s_out_data_5 = TESTS(i).s_out_data_5
                 report "bad result on test: " & integer'image(i + 1)
-                    & ", result: "
-                    & integer'image(to_integer(signed(s_out_data)))
-                    & ", expected: "
-                    & integer'image(to_integer(signed(TESTS(i).s_out_data)))
+                    & ", result32: "
+                    & integer'image(to_integer(signed(s_out_data_32)))
+                    & ", expected32: "
+                    & integer'image(to_integer(signed(TESTS(i).s_out_data_32)))
+                    & ", result5: "
+                    & integer'image(to_integer(signed(s_out_data_5)))
+                    & ", expected5: "
+                    & integer'image(to_integer(signed(TESTS(i).s_out_data_5)))
                 severity error;
         end loop;
-        assert false report "end of test" severity note;
+        report "end of test" severity note;
         clk_kill <= '1';
         -- Wait forever; this will finish the simulation
         wait;
