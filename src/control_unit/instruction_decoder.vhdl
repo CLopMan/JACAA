060fdbd8f15a859764dc4c735919b7f7e2358304
ALVAROPING1 <alvaroping1@gmail.com>
2024-07-14 14:56:09 +0200

    refactor(control_unit/instruction_decoder): replace table entry type with a record

--- src/control_unit/instruction_decoder.vhdl
+++ src/control_unit/instruction_decoder.vhdl
@@ -15,34 +15,39 @@ end entity InstructionDecoder;
 
 
 architecture Rtl of InstructionDecoder is
-    signal current: std_logic_vector(Constants.MICROADDRESS_SIZE downto 0);
+    -- Opcode translation table entry type
+    type opcode_table_entry is record
+        -- Whether this entry corresponds to an invalid instruction
+        invalid: std_logic;
+        -- Address of the microprogram associated with this instruction
+        microaddress: Types.microaddress;
+    end record;
+    signal current: opcode_table_entry;
     signal opcode: std_logic_vector(Constants.OPCODE_SIZE - 1 downto 0);
-    type opcode_index is array(natural range <>) of
-        -- MSB is used to determine if the result is valid
-        std_logic_vector(Constants.MICROADDRESS_SIZE downto 0);
+    type opcode_table is array(natural range <>) of opcode_table_entry;
     -- TODO: fill memory
-    constant OPCODE2MICROADDRESS: opcode_index(0 to 2**Constants.OPCODE_SIZE - 1) := (
-        '0' & x"000",
-        '0' & x"001",
-        '0' & x"002",
-        '0' & x"003",
-        '0' & x"004",
-        '0' & x"005",
-        '0' & x"006",
-        '0' & x"007",
-        '0' & x"008",
-        '0' & x"009",
-        '0' & x"00A",
-        '0' & x"00B",
-        '0' & x"00C",
-        '0' & x"00D",
-        '0' & x"00E",
-        '0' & x"00F",
-        others => ('1' & x"000")
+    constant OPCODE2MICROADDRESS: opcode_table(0 to 2**Constants.OPCODE_SIZE - 1) := (
+        ('0', x"000"),
+        ('0', x"001"),
+        ('0', x"002"),
+        ('0', x"003"),
+        ('0', x"004"),
+        ('0', x"005"),
+        ('0', x"006"),
+        ('0', x"007"),
+        ('0', x"008"),
+        ('0', x"009"),
+        ('0', x"00A"),
+        ('0', x"00B"),
+        ('0', x"00C"),
+        ('0', x"00D"),
+        ('0', x"00E"),
+        ('0', x"00F"),
+        others => ('1', x"000")
     );
 begin
     opcode <= instruction(Constants.OPCODE_SIZE - 1 downto 0);
     current <= OPCODE2MICROADDRESS(to_integer(unsigned(opcode)));
-    microaddress <= current(current'high - 1 downto 0);
-    invalid_instruction <= current(current'high);
+    microaddress <= current.microaddress;
+    invalid_instruction <= current.invalid;
 end architecture Rtl;
