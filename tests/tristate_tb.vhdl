6f1fca3d69bc1e3a4bfc8ec382801a5d4b21bb10
CLopMan <cesarlopman@gmail.com>
2024-07-15 13:06:23 +0200

    extra test and documentation

--- tests/tristate_tb.vhdl
+++ tests/tristate_tb.vhdl
@@ -1,6 +1,6 @@
 library IEEE;
-use IEEE.Std_logic_1164.all;
-use IEEE.Numeric_std.all;
+use IEEE.Std_Logic_1164.all;
+use IEEE.Numeric_Std.all;
 
 library Src;
 use Work.Debug.all;
@@ -17,7 +17,17 @@ architecture Tests of TriStateTB is
     signal s_data_in16, s_data_out16: std_logic_vector(SIZE2 - 1 downto 0)
         := (others => '0');
     signal s_activate: std_logic := '0';
+
+    -- 2 Tristates one Bus
+    signal bus8b: std_logic_vector(SIZE - 1 downto 0)
+        := (others => '0');
+    signal st1: std_logic_vector(SIZE - 1 downto 0)
+        := x"01";
+    signal st2: std_logic_vector(SIZE - 1 downto 0)
+        := x"FF";
+    signal activateT1, activateT2: std_logic := '0';
 begin
+    -- for individual tests diff sizes
     T8: entity Src.Tristate
         generic map(
             data_size => SIZE
@@ -38,6 +48,27 @@ begin
             data_out => s_data_out16
         );
 
+    -- various to one bus
+    T1: entity Src.Tristate
+        generic map(
+            data_size => SIZE
+        )
+        port map(
+            activate => activateT1,
+            data_in => st1,
+            data_out => bus8b
+        );
+
+    T2: entity Src.Tristate
+        generic map(
+            data_size => SIZE
+        )
+        port map(
+            activate => activateT2,
+            data_in => st2,
+            data_out => bus8b
+        );
+
     stim_proc: process
         type test_case is record
             -- inputs
@@ -49,8 +80,17 @@ begin
             data_out16: std_logic_vector(SIZE2 - 1 downto 0);
         end record;
 
+        type test_case2 is record
+            -- inputs
+            activatet1: std_logic;
+            activatet2: std_logic;
+            -- output
+            expected_bus_data: std_logic_vector(SIZE - 1 downto 0);
+        end record;
+        
         type test_arr is array(natural range <>) of test_case;
-        constant tests: test_arr := (
+        type test_arr2 is array(natural range <>) of test_case2;
+        constant indiv_tests: test_arr := (
             -- replicate input in output
             (
                 -- in
@@ -82,33 +122,68 @@ begin
                 std_logic_vector(to_unsigned(10, SIZE2))
             )
         );
+    
+    constant bus_tests: test_arr2 := (
+        -- allow t1
+        (
+            -- input
+            '1', '0',
+            -- output
+            st1
+
+        ),
+        -- allow t2
+        (
+            -- input
+            '0', '1',
+            -- output
+            st2
+        )
+    );
+
     begin
         report "starting triState tests...";
-        for i in tests'range loop
+        for i in indiv_tests'range loop
             report "test: " & integer'image(i + 1);
             -- inputs
-            s_data_in8 <= tests(i).data_in8;
-            s_data_in16 <= tests(i).data_in16;
-            s_activate <= tests(i).activate;
+            s_data_in8 <= indiv_tests(i).data_in8;
+            s_data_in16 <= indiv_tests(i).data_in16;
+            s_activate <= indiv_tests(i).activate;
 
             wait for 10 ns;
             -- output
             --report "debug::: " &  to_string(internal);
-            assert s_data_out16 = tests(i).data_out16
+            assert s_data_out16 = indiv_tests(i).data_out16
                 report "[size2: " & integer'image(SIZE2) & "] failed test "
                     & integer'image(i + 1)
-                    & " expected: " & to_string(tests(i).data_out16)
+                    & " expected: " & to_string(indiv_tests(i).data_out16)
                     & " real: " & to_string(s_data_out16)
                 severity error;
-            assert s_data_out8 = tests(i).data_out8
+            assert s_data_out8 = indiv_tests(i).data_out8
                 report "[size1: " & integer'image(SIZE) & "] failed test "
                     & integer'image(i + 1)
-                    & " expected: " & to_string(tests(i).data_out8)
+                    & " expected: " & to_string(indiv_tests(i).data_out8)
                     & " real: " & to_string(s_data_out8)
                 severity error;
 
         end loop;
+        wait for 10 ns; -- just because
+        -- various triestates connected to one bus
+        report "2 Tristates to one bus";
 
+        for i in bus_tests'range loop
+
+        -- inputs
+        activatet1 <= bus_tests(i).activatet1;
+        activatet2 <= bus_tests(i).activatet2;
+        wait for 10 ns;
+        -- output
+        assert bus8b = bus_tests(i).expected_bus_data
+            report "[2T1B] failed test " & integer'image(i + 1)
+            & " expected: " & to_string(st1)
+            & " real: " & to_string(bus8b);
+
+        end loop;
         report "finishing triState tests...";
         wait;
     end process;
