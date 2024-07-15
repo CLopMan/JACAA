a989227006e3420a19779de91a412df3cada5fe7
CLopMan <cesarlopman@gmail.com>
2024-07-15 13:21:30 +0200

    code style guidelines

--- src/tristate.vhdl
+++ src/tristate.vhdl
@@ -1,6 +1,6 @@
 library IEEE;
-use IEEE.Std_logic_1164.all;
-use IEEE.Numeric_std.all;
+use IEEE.Std_Logic_1164.all;
+use IEEE.Numeric_Std.all;
 use Work.Constants;
 
 entity TriState is
@@ -9,16 +9,18 @@ entity TriState is
     );
     port(
         -- inputs
-        data_in: in std_logic_vector(data_size - 1 downto 0) := (others => '0');
+        data_in: in std_logic_vector(data_size - 1 downto 0)
+            := (others => '0');
         activate: in std_logic := '0';
         -- outputs
-        data_out: out std_logic_vector(data_size - 1 downto 0) := (others => 'Z')
+        data_out: out std_logic_vector(data_size - 1 downto 0)
+            := (others => 'Z')
     );
 end TriState;
 
 architecture Rtl of TriState is
 begin
-    process (activate)
+    process(activate)
     begin
         if activate = '1' then
             data_out <= data_in;
