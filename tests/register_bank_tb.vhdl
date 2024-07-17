93a14b622d9017a72ce168e9f2563b1aba6577c8
ALVAROPING1 <alvaroping1@gmail.com>
2024-07-17 15:23:10 +0200

    fix(register_bank): hardwire zero register to a 0 value

--- tests/register_bank_tb.vhdl
+++ tests/register_bank_tb.vhdl
@@ -39,38 +39,53 @@ begin
         -- The patterns to apply
         type tests_array is array (natural range <>) of tests_case;
         constant TESTS: tests_array := (
-            (
+            ( -- 1: Set C input but don't load value
                 "00000", "00001", "00001", '0',
                 x"0000007f",
                 x"00000000", x"00000000"
             ),
-            (
+            ( -- 2: Set C input and load value
                 "00000", "00001", "00001", '1',
                 x"0000007f",
                 x"00000000", x"0000007f"
             ),
-            (
+            ( -- 3: Modify C input and load value again
                 "00000", "00001", "00001", '1',
                 x"7f000000",
                 x"00000000", x"7f000000"
             ),
-            (
+            ( -- 4: Load different value into a different register and read both
                 "00010", "00001", "00010", '1',
                 x"11111111",
                 x"11111111", x"7f000000"
             ),
-            (
+            ( -- 5: Modify C input but don't load value
                 "00010", "00001", "00010", '0',
                 x"00ff0000",
                 x"11111111", x"7f000000"
             ),
-            (
+            ( -- 6: Read the same register through A and B
                 "00010", "00010", "00010", '0',
                 x"00000000",
                 x"11111111", x"11111111"
             ),
-            (
+            ( -- 7: Read older values stored in different registers
                 "00001", "00000", "00010", '0',
+                x"00000000",
+                x"7f000000", x"00000000"
+            ),
+            ( -- 8: Read zero register
+                "00000", "00001", "00000", '0',
+                x"00000000",
+                x"00000000", x"7f000000"
+            ),
+            ( -- 9: Try to modify the zero register
+                "00000", "00001", "00000", '1',
+                x"33333333",
+                x"00000000", x"7f000000"
+            ),
+            ( -- 10: Read zero register again
+                "00001", "00000", "00000", '0',
                 x"00000000",
                 x"7f000000", x"00000000"
             )
