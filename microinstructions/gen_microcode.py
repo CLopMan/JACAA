846a250916beab9dc48dae13737e8e653a6feb04
CLopMan <cesarlopman@gmail.com>
2024-07-31 13:16:25 +0200

    i forgot to update a counter

--- microinstructions/gen_microcode.py
+++ microinstructions/gen_microcode.py
@@ -94,6 +94,7 @@ def parse_csv(file_name: str):
         if ins == tuple(cpi.keys())[-1]:
             microcomma = ""
         code_to_micro += f"{opcodes[ins]} => ('0',x\"{hex(ins_count)[2:].rjust(3, '0')}\"){microcomma}\t-- Opcode {bin(opcodes[ins])[2:].rjust(7, "0")}\n"
+        ins_count += cpi[ins]
     fd.close()
     return control_memory, code_to_micro
 
